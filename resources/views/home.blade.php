a145777a7d8cf9a8695782ef21053f31626ae875
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-29 11:56:43 -0700

    adds apartment form and listings to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <apartment-form></apartment-form>
+            <div class="row">
+                <div class="col">
+                    <apartment-listings :apartments="{{ $apartments }}"></apartment-listings>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <apartment-form></apartment-form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
