53a07fba95c641e4e4bb576f357ae12de2980e7c
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-29 12:59:47 -0700

    adds filename, url, accessors

--- app/Apartment.php
+++ app/Apartment.php
@@ -2,8 +2,11 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
 use App\Events\ApartmentSaving;
+use Stevebauman\Purify\Facades\Purify;
 use Illuminate\Database\Eloquent\Model;
+use VerumConsilium\Browsershot\Facades\Screenshot;
 
 class Apartment extends Model
 {
@@ -15,6 +18,7 @@ class Apartment extends Model
     protected $fillable = [
         'email',
         'url',
+        'user_id',
     ];
 
     /**
@@ -25,4 +29,44 @@ class Apartment extends Model
     protected $dispatchesEvents = [
         'saving' => ApartmentSaving::class,
     ];
+
+    /**
+     * Take a screenshot of the page at the URL, and
+     * save to the public screenshots directory.
+     *
+     * @return string|false
+     */
+    public function takeScreenshot()
+    {
+        $screenshot = Screenshot::loadUrl($this->url)
+            ->useJPG()
+            ->windowSize(1920, 1080)
+            ->waitUntilNetworkIdle()
+            ->dismissDialogs();
+
+        return $screenshot->storeAs('public/screenshots', $this->filename);
+    }
+
+    /**
+     * Get the full path to the image for the apartment listing.
+     *
+     * @return string
+     */
+    public function getFilenameAttribute()
+    {
+        return Str::kebab("{$this->id}.jpg");
+    }
+
+    /**
+     * Url attribute accessor.
+     * Sanitzes the url attribute since it includes user input.
+     *
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getUrlAttribute($url)
+    {
+        return Purify::clean($url);
+    }
 }
