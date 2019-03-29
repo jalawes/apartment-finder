7223283df9dfa1c7529d827e3992d599ee1d86dd
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-29 13:06:18 -0700

    updates gitignore

---
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreApartmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'url' => 'required|url',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.email' => 'The email must be a valid email address',
+            'email.required' => 'A email is required',
+            'url.required'  => 'A url is required',
+            'url.url'  => 'A url must be a valid url',
+        ];
+    }
+}
