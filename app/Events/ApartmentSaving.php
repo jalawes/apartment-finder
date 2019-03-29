59f744c34448cc7bbbdf7d35f1d6090302642f4b
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-29 12:16:40 -0700

    moves event to proper directory

---
+++ app/Events/ApartmentSaving.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events;
+
+use App\Apartment;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class ApartmentSaving
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * @var \App\Apartment
+     */
+    protected $apartment;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param mixed $apartment
+     * @return void
+     */
+    public function __construct(Apartment $apartment)
+    {
+        $this->apartment = $apartment;
+    }
+}
