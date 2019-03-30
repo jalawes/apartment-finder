2039593169eb2de52aee2b7a2f01f9f5ef319a5e
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-29 22:59:26 -0700

    adds moment component into listing card

--- resources/js/components/ApartmentListings.vue
+++ resources/js/components/ApartmentListings.vue
@@ -1,34 +1,27 @@
 <template>
-  <div id="apartment-listing-controls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://via.placeholder.com/728x150.png?text=Your-Applied-Apartment+Listing+Image+1" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/728x150.png?text=Your-Applied-Apartment+Listing+Image+2" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/728x150.png?text=Your-Applied-Apartment+Listing+Image+3" class="d-block w-100" alt="...">
-      </div>
+<div class="card-deck">
+  <div class="card" v-for="apartment in recentApartments" :key="apartment.id">
+    <img :src="apartment.thumbnail_path" class="card-img-top" alt="Recent Apartment Listing">
+    <div class="card-body">
+      <p class="card-text"><small class="text-muted">Last updated <moment :date="apartment.updated_at" :ago="true"></moment>.</small></p>
     </div>
-    <a class="carousel-control-prev" href="#apartment-listing-controls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#apartment-listing-controls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
   </div>
+</div>
 </template>
 
 <script>
+
 export default {
   name: 'apartment-listings',
   props: {
     apartments: {
       required: true,
       type: Array,
+    },
+  },
+  computed: {
+    recentApartments() {
+      return this.apartments.slice(0, 3);
     },
   },
 };
