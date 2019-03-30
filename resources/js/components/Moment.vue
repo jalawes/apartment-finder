1f006d26a311eb437bc548e01551fb745b3e1c25
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-29 22:59:11 -0700

    adds moment component

---
+++ resources/js/components/Moment.vue
@@ -0,0 +1,33 @@
+<template>
+  <span>
+    <span v-if="ago">{{ calendarTime }}</span>
+    <span v-text="now" v-else></span>
+  </span>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'moment',
+  props: {
+    ago: {
+      required: false,
+      type: Boolean,
+      default: true,
+    },
+    date: {
+      required: true,
+      type: String,
+    },
+  },
+  computed: {
+    calendarTime() {
+      return moment().from(this.date);
+    },
+    now() {
+      return moment();
+    },
+  },
+};
+</script>
