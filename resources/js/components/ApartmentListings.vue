47e59df8aa21731b495dba005b279d4499600e7f
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-30 10:17:19 -0700

    chunks apartments into rows

--- resources/js/components/ApartmentListings.vue
+++ resources/js/components/ApartmentListings.vue
@@ -1,15 +1,18 @@
 <template>
-<div class="card-deck">
-  <div class="card" v-for="apartment in recentApartments" :key="apartment.id">
-    <img :src="apartment.thumbnail_path" class="card-img-top" alt="Recent Apartment Listing">
-    <div class="card-body">
-      <p class="card-text"><small class="text-muted">Last updated <moment :date="apartment.updated_at" :ago="true"></moment>.</small></p>
+  <div>
+    <div class="card-deck" v-for="chunk in recentApartments">
+      <div class="card listing-card-deck" v-for="apartment in chunk" :key="apartment.id">
+        <img :src="apartment.thumbnail_path" class="card-img-top " alt="Recent Apartment Listing">
+        <div class="card-footer">
+          <small class="text-muted">Last updated <moment :date="apartment.updated_at" :ago="true"></moment>.</small>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
+const chunk = (arr, size) => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size));
 
 export default {
   name: 'apartment-listings',
@@ -21,7 +24,7 @@ export default {
   },
   computed: {
     recentApartments() {
-      return this.apartments.slice(0, 3);
+      return chunk(this.apartments, 3);
     },
   },
 };
