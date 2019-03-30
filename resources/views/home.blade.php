7dec57ae9c82a1eb7a492b93035d1c54afc04162
Jamal Alawes <jamal.alawes@gmail.com>
2019-03-30 11:04:52 -0700

    moves form to sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="row">
-                <div class="col">
-                    <apartment-listings :apartments="{{ $apartments }}"></apartment-listings>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <apartment-form></apartment-form>
-                </div>
-            </div>
+        <div class="col-3">
+            <apartment-form></apartment-form>
+        </div>
+        <div class="col">
+            <apartment-listings :apartments="{{ $apartments }}"></apartment-listings>
         </div>
     </div>
 </div>
