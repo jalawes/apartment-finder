4845afbfa8f5c804a6e40bb5019d7083eef2860f
Jamal Alawes <jamal.alawes@gmail.com>
2019-04-01 16:06:07 -0700

    adds helpers to testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,39 @@
 
 namespace Tests;
 
+use App\User;
+use Faker\Factory as Faker;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->faker = Faker::create();
+    }
+
+    /**
+     * Sign in and act as a user.
+     *
+     * @param \App\Models\User|\Illuminate\Database\Eloquent\Model $user
+     * @param null|mixed $guard
+     *
+     * @return $this
+     */
+    public function signIn($user = null, $guard = null)
+    {
+        $user = $user ?? create(User::class);
+
+        $this->actingAs($user, $guard);
+
+        return $this;
+    }
 }
