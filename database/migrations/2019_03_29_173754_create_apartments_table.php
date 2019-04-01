8c3260e5b8cb728dd8c6a288867ea76506f0deed
Jamal Alawes <jamal.alawes@gmail.com>
2019-04-01 16:44:33 -0700

    makes URLs unique

--- database/migrations/2019_03_29_173754_create_apartments_table.php
+++ database/migrations/2019_03_29_173754_create_apartments_table.php
@@ -17,7 +17,7 @@ class CreateApartmentsTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedInteger('user_id');
             $table->string('email');
-            $table->string('url');
+            $table->string('url')->unique();
             $table->timestamps();
         });
     }
