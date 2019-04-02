4888fdf012fa87cd162d7afb5a9b3ed52587bc30
Jamal Alawes <jamal.alawes@gmail.com>
2019-04-01 18:20:55 -0700

    combines comps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-3">
-            <apartment-form></apartment-form>
-        </div>
-        <div class="col">
-            <apartment-listings :apartments="{{ $apartments }}"></apartment-listings>
-        </div>
-    </div>
+    <apartment-listings :apartments="{{ $apartments }}"></apartment-listings>
 </div>
 @endsection
