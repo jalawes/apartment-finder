1d081e105c349a17768d8a2a852531545351349d
Jamal Alawes <jamal.alawes@gmail.com>
2019-04-01 17:17:55 -0700

    updates scss class

--- resources/js/components/ApartmentListings.vue
+++ resources/js/components/ApartmentListings.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="card-deck" v-for="chunk in recentApartments">
-      <div class="card listing-card-deck" v-for="apartment in chunk" :key="apartment.id">
+      <div class="card card-deck-card" v-for="apartment in chunk" :key="apartment.id">
         <img :src="apartment.thumbnail_path || 'https://via.placeholder.com/253x189.jpg?text=No+Photo'" class="card-img-top " alt="Recent Apartment Listing">
         <div class="card-footer">
           <small class="text-muted">Last updated <moment :date="apartment.updated_at" :ago="true"></moment>.</small>
