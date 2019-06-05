79b19df82675a9ddf3cd76e4723ea472874fdf35
Jamal Alawes <jamal.alawes@gmail.com>
2019-06-05 16:04:00 -0700

    extracts mail template

--- resources/views/emails/apartments/found.blade.php
+++ resources/views/emails/apartments/found.blade.php
@@ -1,23 +1,11 @@
 @component('mail::message')
-Hi,
-
-I am a UBC Alumnus, now working as a software developer in downtown Vancouver.
-
-I’ve recently come across the listing you posted online, and I was wondering if it's still available? I'm looking for a place for myself and my cat.
-
-Whenever it's convenient for you, it would be great if we could have a conversation in person or, if it's better for you, we could continue a discussion online.
-
-I'm available after 5:00 PM weekdays and any time of the day during the weekend. However, my schedule is quite flexible so I'm able to accommodate a time that works for you.
-
-I’m very interested in learning more, and eager to hear back from you. Please feel free to message me and I will get back to you as soon as I can.
-
-Thank you for your consideration,
+{{ config('mail.templates.apartments.found') }}
 <br>
 <br>
-Jamal Alawes
+{{ config('mail.from.name') }}
 <br>
-jamal.alawes@me.com
-
+{{ config('mail.from.address') }}
+<br>
 <br>
 {{ $apartment->url }}
 @endcomponent
